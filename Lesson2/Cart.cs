0914b48651667dcd88a2c06236b8169524da4613
Werizon <bogdanskafa@gmail.com>
2021-05-19 16:22:42 +0300

    Models

---
+++ Lesson2/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_NIX_2.Lesson2
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public int AvtoId { get; set; }
+        public int UserOrderId { get; set; }
+        public string StatusCart { get; set; }
+
+        public Avto avto { get; set; }
+        public Order Order { get; set; }
+    }
+}
---
+++ Lesson2/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_NIX_2.Lesson2
+{
+    public class Order
+    {
+        public int AvtoId { get; set; }
+        public int UserId { get; set; }
+        public int Count { get; set; }
+        public float Price { get; set; }
+        public User User { get; set; }
+        public Avto avto { get; set; }
+
+        public Order(int count, float price)
+        {
+            Count = count;
+            Price = price;
+        }
+
+    }
+}
