d24e63b600ea88007d01082a47f6ab03fb671eb0
Werizon <bogdanskafa@gmail.com>
2021-05-19 15:11:54 +0300

    Добавьте файлы проекта.

---
+++ Lesson2/Avto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_NIX_2.Lesson2
+{
+    class Avto
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Body { get; set; }
+        public int ReleaseYear { get; set; }
+        public int LiftingCapacity { get; set; }
+        public float Price { get; set; }
+
+
+
+
+    }
+}
